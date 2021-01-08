18fba1a56bcbf8565747d9c8d4f12ea8938e750d
Andrew Garshyn <garshyn@gmail.com>
2021-01-08 14:48:16 +0100

    simplecov installed

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ group :development, :test do
   gem 'factory_bot_rails'
   gem 'rspec-rails', '~> 4.0.0'
   gem 'shoulda-matchers'
+  gem 'simplecov', require: false
 end
 
 group :development do
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,6 +8,9 @@ require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 require 'factory_bot_rails'
+require 'simplecov'
+SimpleCov.start 'rails' do
+end
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
