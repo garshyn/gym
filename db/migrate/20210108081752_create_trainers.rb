678619d04e1db6bcfc2de5ef37488570da2cfe79
Andrew Garshyn <garshyn@gmail.com>
2021-01-08 14:40:09 +0100

    Trainer model created

---
+++ db/migrate/20210108081752_create_trainers.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class CreateTrainers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :trainers do |t|
+      t.string :first_name
+      t.string :last_name
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/trainers.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :trainer do
+    first_name { 'MyString' }
+    last_name { 'MyString' }
+  end
+end
